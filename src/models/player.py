d12403010f87f60fa103e4a7a33c249f03ca10f0
Veronika <veronikasventik@gmail.com>
2024-02-27 11:34:29 +0100

    Implemented make_move method in Player class

--- src/models/player.py
+++ src/models/player.py
@@ -8,10 +8,23 @@ class Player:
 
         self.name = name
         self.symbol = symbol # 1 = X or 2 = O
-        self.game_stats = GameStats(name) # Game Stats noch nicht vorhanden
+        #self.game_stats = GameStats(name) # Game Stats noch nicht vorhanden
 
     def make_move(self, game, row: int, col: int):
-        return 1
+        if game.player == self.symbol:  # check ob der Spieler an der Reihe ist
+            index = row * 3 + col
+            # aktuelles board abrufen
+            if game.board[index // 3][index % 3] == 0:  # Feld ist frei
+                game.handle_turn(index, game.board)
+                return True
+            else:
+                return False
+            # TODO: Check wenn das Spiel vorbei ist
+            if game.check_winner():
+                self.update_game_stats(winner=True)
+                # TODO: Gewinnmeldung
+            else:
+                self.update_game_stats(winner=False)
     
     def update_game_stats(self, winner: bool):
         return 1
