751b194ed5ea2c63a1092ec987b2b620c30a5947
Veronika <veronikasventik@gmail.com>
2024-02-29 13:06:41 +0100

    Refactor make_move method in Player class

--- src/models/player.py
+++ src/models/player.py
@@ -13,12 +13,9 @@ class Player:
     def make_move(self, game, row: int, col: int):
         if game.player == self.symbol:  # check ob der Spieler an der Reihe ist
             index = row * 3 + col
-            # aktuelles board abrufen
-            if game.board[index // 3][index % 3] == 0:  # Feld ist frei
-                game.handle_turn(index, game.board)
-                return True
-            else:
-                return False
+            # handle auslösen
+            game.handle_turn(index, game.board)
+
             # TODO: Check wenn das Spiel vorbei ist
             if game.check_winner():
                 self.update_game_stats(winner=True)
