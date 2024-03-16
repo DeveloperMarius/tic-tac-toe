fa18d81b60b401b083a321d5d8611c654a7d3e82
Marius Karstedt <info@marius-karstedt.de>
2024-03-16 16:49:51 +0100

    changed dev test files

--- test_socket_server.py
+++ test_socket_server.py
@@ -1,9 +1,13 @@
+import time
+
 from main import Main
 from src.game.network import NetworkServer, NetworkClient
 import random
 
-with NetworkServer() as s:
+with NetworkServer.get_instance() as s:
     usernames = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10']
-    c = NetworkClient(random.choice(usernames))
-    c.connect()
-    Main()
+    c = NetworkClient()
+    c.connect(random.choice(usernames), '127.0.0.1')
+    while True:
+        time.sleep(5)
+    # Main()
